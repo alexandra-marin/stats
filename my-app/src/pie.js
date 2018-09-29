44a96a713b0dfc7a84c3a112527c58fd74875e82
Alexandra Marin <alexandra.marin01@gmail.com>
2018-09-29 16:17:22 -0400

    first commit

---
+++ my-app/src/pie.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react';
+import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector,
+  Label, LabelList } from 'recharts';
+import { scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+import _ from 'lodash';
+
+const colors = scaleOrdinal(schemeCategory10).range();
+
+let dau = 300;
+let wau = 300;
+let mau = 300;
+
+const data02 = [
+    { name: 'Daily', value: mau },
+    { name: 'Weekly', value: wau },
+    { name: 'Monthly', value: dau }
+];
+
+const initialState = { data02 };
+
+const renderLabelContent = (props) => {
+  const { value, percent, x, y, midAngle } = props;
+
+  return (
+    <g transform={`translate(${x}, ${y})`} textAnchor={ (midAngle < -90 || midAngle >= 90) ? 'end' : 'start'}>
+      <text x={0} y={0}>{`Count: ${value}`}</text>
+      <text x={0} y={20}>{`(Percent: ${(percent * 100).toFixed(2)}%)`}</text>
+    </g>
+  );
+};
+
+  
+const changeNumberOfData = (data) => {
+    if (Array.isArray(data)) {
+      return data.map(changeNumberOfData);
+    }
+  
+    if (typeof data === 'object') {
+      return _.mapValues(data, val => {
+        
+        console.log(data, val, dau);
+        if(data.name === 'Daily') return parseInt(dau);
+        if(data.name === 'Weekly') return parseInt(wau);
+        if(data.name === 'Monthly') return parseInt(mau);
+        
+        if (typeof val === 'number') {
+          return parseInt(500);
+        }
+  
+        return changeNumberOfData(val);
+      });
+    }
+  
+    return data;
+  }
+
+export default class PeerioPieChart extends Component {
+
+  static displayName = 'PieChartDemo';
+
+  onPieEnter = (data, index, e) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+
+  state = {
+    ...initialState,
+    activeIndex: 0,
+    animation: false,
+  };
+
+  handleChangeData = () => {
+    this.setState(() => _.mapValues(initialState, changeNumberOfData));
+    // this.setState(() => {
+    //     this.state.initialState = {
+    //         data02 : [
+    //             { name: 'Daily', value: this.mau },
+    //             { name: 'Weekly', value: this.wau },
+    //             { name: 'Monthly', value: this.dau }
+    //         ]
+    //     }
+    // });
+    // this.forceUpdate();
+  };
+
+  handleChangeAnimation = () => {
+    this.setState({
+      animation: !this.state.animation,
+    });
+  };
+
+  handlePieChartEnter = (a, b, c) => {
+    console.log(a, b, c);
+  };
+
+  handleEnter = (e, activeIndex) => this.setState({ activeIndex });
+  handleLeave = () => this.setState({ activeIndex: -1 });
+
+  handleDau = (event) => {
+    dau = event.target.value;
+  }
+  handleWau= (event) => {
+    wau = event.target.value;
+  }
+  handleMau= (event) => {
+    mau = event.target.value;
+  }
+
+  render () {
+    const { data02 } = this.state;
+
+    return (
+      <div className="pie-charts">
+      <form onSubmit={(event) => event.preventDefault()}>
+        <label>
+            DAU:
+            <input type="text"  onChange={this.handleDau} />
+        </label>
+        <label>
+            WAU:
+            <input type="text" onChange={this.handleWau} />
+        </label>
+        <label>
+            MAU:
+            <input type="text"  onChange={this.handleMau} />
+        </label>
+        </form>
+        <a
+          href="javascript: void(0);"
+          className="btn update"
+          onClick={this.handleChangeData}
+        >
+          change data
+        </a>
+        <br/>
+        <p>Simple PieChart</p>
+        <div className="pie-chart-wrapper">
+          <button onClick={this.handleChangeAnimation}>change animation</button>
+          <PieChart width={800} height={400}>
+            <Legend />
+            <Pie
+              data={data02}
+              dataKey="value"
+              cx={600}
+              cy={200}
+              startAngle={180}
+              endAngle={-180}
+              innerRadius={60}
+              outerRadius={80}
+              label={renderLabelContent}
+              paddingAngle={5}
+              isAnimationActive={this.state.animation}
+            >
+              {
+                data02.map((entry, index) => (
+                  <Cell key={`slice-${index}`} fill={colors[index % 10]}/>
+                ))
+              }
+              <Label width={50} position="center">
+                Users
+              </Label>
+            </Pie>
+          </PieChart>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
